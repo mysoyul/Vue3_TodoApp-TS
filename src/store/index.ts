1aff260bb289af2a7fd94fb7e662ce147b854b12
mysoyul <vega2k_@naver.com>
2023-06-23 14:47:52 +0900

    store module 화

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,94 +1,18 @@
 import { createStore, createLogger } from "vuex"
-import axios from "axios"
+import { moduleTodo, ModuleTodoState } from './modules/moduleTodo'
 
-import TodoItem from "@/types/TodoItem"
-import http from "@/common/http-common"
-
-//State 타입 선언
-export type State = { todoItems: TodoItem[] };
-
-const state: State = { todoItems: [] };
+export interface RootState {
+    moduleTodo: ModuleTodoState
+}
 
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
         [createLogger()] : [],
-    state,
-    actions: {
-        loadTodoItems({ commit }) {
-            http
-                .get('/todos')
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-                .catch(error => {
-                    console.log(error)
-                    if (axios.isAxiosError(error)) {
-                        console.log(error?.response?.status +
-                            ' : ' + error.message)
-                    } else {
-                        console.error(error);
-                    }
-                });
-        },
-        removeTodo({ commit }, payload: TodoItem) {
-            http
-                .delete(`/todos/${payload.id}`)
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        },
-        addTodo({ commit }, payload: TodoItem) {
-            http
-                .post(`/todos`, payload)
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        },
-        toggleTodo({ commit }, payload: TodoItem) {
-            http
-                .patch(`/todos/${payload.id}`, payload)
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        },
-        clearTodo({ commit }) {
-            http
-                .delete('/todos')
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        },
-    },
-    mutations: {
-        setTodoItems(state, items) {
-            state.todoItems = items;
-        },
-        addTodo(state, todoItemStr: string) {
-            const obj: TodoItem = { completed: false, item: todoItemStr };
-            localStorage.setItem(todoItemStr, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeTodo(state, payload) {
-            const { todoItem: { item }, index } = payload
-            localStorage.removeItem(item);
-            state.todoItems.splice(index, 1);
-        },
-        toggleTodo(state, payload) {
-            const { todoItem: { item, completed }, index } = payload
-            state.todoItems[index].completed = !completed
-            localStorage.removeItem(item);
-            localStorage.setItem(item, JSON.stringify(state.todoItems[index]));
-        },
-        clearTodo(state) {
-            localStorage.clear()
-            state.todoItems = []
-        }
-    },
+    modules: {
+        moduleTodo
+    }
+
+
 })
 
 
---
+++ src/store/modules/moduleTodo.ts
@@ -0,0 +1,93 @@
+import { Module } from "vuex"
+import axios from "axios"
+
+import { RootState } from "../index"
+import TodoItem from "@/types/TodoItem"
+import http from "@/common/http-common"
+
+export interface ModuleTodoState {
+    todoItems: TodoItem[];
+}
+
+export const moduleTodo: Module<ModuleTodoState, RootState> = {
+    namespaced: true,
+    state: () => ({
+        todoItems: []
+    }), //state
+    actions: {
+        loadTodoItems({ commit }) {
+            http
+                .get('/todos')
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+                .catch(error => {
+                    console.log(error)
+                    if (axios.isAxiosError(error)) {
+                        console.log(error?.response?.status +
+                            ' : ' + error.message)
+                    } else {
+                        console.error(error);
+                    }
+                });
+        },
+        removeTodo({ commit }, payload: TodoItem) {
+            http
+                .delete(`/todos/${payload.id}`)
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+        },
+        addTodo({ commit }, payload: TodoItem) {
+            http
+                .post(`/todos`, payload)
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+        },
+        toggleTodo({ commit }, payload: TodoItem) {
+            http
+                .patch(`/todos/${payload.id}`, payload)
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+        },
+        clearTodo({ commit }) {
+            http
+                .delete('/todos')
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+        },
+    },//actions
+    mutations: {
+        setTodoItems(state, items) {
+            state.todoItems = items;
+        },
+        addTodo(state, todoItemStr: string) {
+            const obj: TodoItem = { completed: false, item: todoItemStr };
+            localStorage.setItem(todoItemStr, JSON.stringify(obj));
+            state.todoItems.push(obj);
+        },
+        removeTodo(state, payload) {
+            const { todoItem: { item }, index } = payload
+            localStorage.removeItem(item);
+            state.todoItems.splice(index, 1);
+        },
+        toggleTodo(state, payload) {
+            const { todoItem: { item, completed }, index } = payload
+            state.todoItems[index].completed = !completed
+            localStorage.removeItem(item);
+            localStorage.setItem(item, JSON.stringify(state.todoItems[index]));
+        },
+        clearTodo(state) {
+            localStorage.clear()
+            state.todoItems = []
+        }
+    },
+}
