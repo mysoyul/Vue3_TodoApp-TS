4f4fe4ef4dd34d350c605665b2d4779f1cb28ddc
mysoyul <vega2k_@naver.com>
2023-06-23 16:34:26 +0900

    modulePost.ts add

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 8081
+    port: 8081,
+    client: {
+      overlay: false
+    }
   }
 })
